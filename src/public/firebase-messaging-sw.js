a6cf29ed2b861cdd54f5ebe58194fb917f4c5d4b
Ikhsan Asa P <ikhsan.pambayun@gmail.com>
2025-06-29 16:38:02 +0700

    new branch

--- src/public/firebase-messaging-sw.js
+++
@@ -1,9 +0,0 @@
-
-self.addEventListener('push', (event) => {
-    const data = event.data.json();
-    const title = data.title;
-    const options = data.options;
-  
-    event.waitUntil(self.registration.showNotification(title, options));
-  });
-  
